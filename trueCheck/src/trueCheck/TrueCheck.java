0ab8c140716de6e592c3664ff503ed782475e703
Gloria <Gloria@Gloria-PC>
2023-01-02 00:48:43 +1300

    FIRST COMMIT

---
+++ trueCheck/src/trueCheck/TrueCheck.java
@@ -0,0 +1,10 @@
+package trueCheck;
+import java.util.Scanner;
+public class TrueCheck {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number");
+		System.out.println(sc.hasNextInt());
+	}
+
+}
